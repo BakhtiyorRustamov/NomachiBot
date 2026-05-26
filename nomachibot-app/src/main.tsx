3861f05296fd569ff3d27b2a94d81a3f00be6ce3
bakhtiyorrustamov <bakhtiyor1chi@gmail.com>
2026-05-26 00:10:44 -0400

    feat: configure axios base URL using Vite environment variables

--- nomachibot-app/src/main.tsx
+++ nomachibot-app/src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n'
 import App from './App.tsx'
+import axios from 'axios'
 
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
